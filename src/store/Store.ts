fc14a050fa9c72cfb34987a47dfc8b396375cbbb
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2025-01-21 18:42:51 +0000

    store.ts updated

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,27 +1,32 @@
+
 export type UserId = string;
 
-export interface Chat{
+export interface Chat {
     id: string;
-    userId:UserId;
-    name:string;
-    message:string;
-    upvotes:string[]; // will contain who has upvoted what 
+    userId: UserId;
+    name: string;
+    message: string;
+    upvotes: UserId[]; // who has upvoited what 
 }
 
-export abstract class Store{
-    constructor(){
+export abstract class Store {
+    constructor() {
 
     }
-    initRoom(roomId:string){
+    initRoom(roomId: string) {
 
     }
-    getChats(room : string, limit: number,offset:number){
+
+    getChats(room: string, limit: number, offset: number) {
 
     }
-    addChat(userId:UserId,name:string,room : string,  message:string){
+
+    addChat(userId: UserId, name: string, room: string, message: string) {
 
     }
-    upvote(userId:UserId,room:string, chatId:string){
+
+    upvote(userId: UserId, room: string, chatId: string) {
 
     }
+
 }
