1869dce6e3a4d239e045b13e5cd880e6a4d3c171
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2025-01-21 18:42:41 +0000

    messages folder updated

--- src/messages/incomingMessages.ts
+++ src/messages/incomingMessages.ts
@@ -1,13 +1,13 @@
-import z from 'zod';
 
+import z from "zod";
 
-export enum SupportedMessage{
-    JoinRoom = "JOIN_ROOM",
+export enum SupportedMessage {
+    JoinRoom =  "JOIN_ROOM",
     SendMessage = "SEND_MESSAGE",
-    UpvoteMessage = "UPVOTE_MESSAGE"
+    UpvoteMessage = "UPVOTE_MESSAGE",   
 }
 
-export type IncomingMessages = {
+export type IncomingMessage = {
     type: SupportedMessage.JoinRoom,
     payload: InitMessageType
 } | {
@@ -18,30 +18,26 @@ export type IncomingMessages = {
     payload: UpvoteMessageType
 };
 
-
 export const InitMessage = z.object({
     name: z.string(),
-    userId:z.string(),
-    roomId:z.string(),
+    userId: z.string(), 
+    roomId: z.string(),
 })
 
 export type InitMessageType = z.infer<typeof InitMessage>;
 
-
 export const UserMessage = z.object({
-    userId:z.string(),
-    roomId:z.string(),
+    userId: z.string(),
+    roomId: z.string(),
     message: z.string()
 })
 
 export type UserMessageType = z.infer<typeof UserMessage>;
 
-
 export const UpvoteMessage = z.object({
-    userId:z.string(),
-    roomId:z.string(),
+    userId: z.string(),
+    roomId: z.string(),
     chatId: z.string()
 })
-
 
 export type UpvoteMessageType = z.infer<typeof UpvoteMessage>;
--- src/messages/outgoingMessages.ts
+++ src/messages/outgoingMessages.ts
@@ -1,26 +1,21 @@
 
-
 export enum SupportedMessage {
-    AddChat = "ADD_CHAT",
+    AddChat =  "ADD_CHAT",
     UpdateChat = "UPDATE_CHAT",
 }
 
-
 type MessagePayload = {
-    roomId: string,
-    message: string,
-    name: string,
-    upvotes: number,
-    chatId: string
+    roomId: string;
+    message: string;
+    name: string;
+    upvotes: number;
+    chatId: string;
 }
-
 
 export type OutgoingMessage = {
     type: SupportedMessage.AddChat,
     payload: MessagePayload
 } | {
     type: SupportedMessage.UpdateChat,
-    payload: Partial <MessagePayload>
+    payload: Partial<MessagePayload>
 }
-
-
